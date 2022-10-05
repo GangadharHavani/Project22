a4ecd2eac2d1d40265e98b6624b961031f725a74
Gangadhar Havani <gangadhar.havani33@gmail.com>
2022-10-05 15:11:57 +0530

    commit message

---
+++ sumOfTwoNumbers.js
@@ -0,0 +1,10 @@
+let num1 = 5
+let num2 = 5
+let sum = (num1+num2);
+console.log("-------- Sum Of Two Numbers ---------");
+if(num1==num2){
+    console.log("The Sum of the given two numbers is: ",sum*3);
+}
+else{
+    console.log("The Sum of the given two numbers is: ",sum);
+}
